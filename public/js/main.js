7e756ea24549b5ac0031d1d5ba6b9df19f6799bb
Shidomo <shindor.hardcore@gmail.com>
2022-12-02 23:31:27 +0200

    add little bit js for change content after click or chexbox-menu

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,82 @@
-console.log(1);
-console.log(1);
-console.log(1);
-console.log(1);
+var first = document.getElementById("consistent-text");
+var second = document.getElementById("customer-oriented-text");
+var third = document.getElementById("innovative-text");
+var four = document.getElementById("business-oriented-text");
+let check = document.querySelectorAll('.checbox-items');
+let lastClicked = check[0];
+
+
+
+
+for( let i = 0; i < check.length; i++ ){
+
+  check[i].addEventListener('click', function(){
+    lastClicked.classList.remove('linear-checkbox');
+    this.classList.add('linear-checkbox');
+    lastClicked = this; 
+  });
+}
+
+
+function consistent() {
+    if (first.style.display === "none") {
+        first.style.display = "block";
+        second.style.display = "none";
+        third.style.display = "none";
+        four.style.display = "none";
+
+    }else {
+        first.style.display = "block";
+        second.style.display = "none";
+        third.style.display = "none";
+        four.style.display = "none";
+    }
+}
+
+
+
+function customer_oriented() {
+
+    if (second.style.display === "none") {
+        
+        second.style.display = "block"
+        first.style.display = "none"
+        third.style.display = "none"
+        four.style.display = "none";
+
+    }else {
+        second.style.display = "block"
+        first.style.display = "none"
+        third.style.display = "none"
+        four.style.display = "none";
+    }
+}
+
+function innovative() {
+
+    if (third.style.display === "none") {
+        third.style.display = "block";
+        first.style.display = "none";
+        second.style.display = "none";
+        four.style.display = "none";
+    }
+    else {
+        third.style.display = "block";
+        first.style.display = "none";
+        second.style.display = "none";
+        four.style.display = "none";
+    }
+}
+function business_oriented() {
+    if (four.style.display === "none") {
+        four.style.display = "block";
+        first.style.display = "none";
+        second.style.display = "none";
+        third.style.display = "none";
+
+    }
+    else {
+        four.style.display = "block";
+
+    }
+}
